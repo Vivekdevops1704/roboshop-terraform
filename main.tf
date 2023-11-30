03474343512a15255e2797f301cbe2c47756cc5b
Vivekdevops1704 <150699648+Vivekdevops1704@users.noreply.github.com>
2023-11-30 19:39:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module components {
-    source          =   "git:https://github.com/Vivekdevops1704/tf-module-basic-test.git"
+    source          =   "git::https://github.com/Vivekdevops1704/tf-module-basic-test.git"
     for_each        =    var.components
     zone_id         =    var.zone_id
     security_groups =    var.security_groups
