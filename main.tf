5ad44d58860bd3ba3e6ae1faf927b265306f04bf
vivek <vivekemails17@gmail.com>
2023-12-02 01:37:18 +0530

    added output

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
-module "components" {
+module "vpc" {
     source          =   "git::https://github.com/Vivekdevops1704/tf-module-vpc.git"
     for_each        =    var.vpc
     cidr            =    each.value["cidr"]
-    subnets          =    each.value["subnets"]
+    subnets         =    each.value["subnets"]
+}
 
-
+output "vpc" {
+  value = module.vpc
 }
