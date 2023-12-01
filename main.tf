1d575eb14033ecba1b0fe4ffd4632ef344dfa0ad
vivek <vivekemails17@gmail.com>
2023-12-01 23:56:51 +0530

    added files

--- main.tf
+++ main.tf
@@ -2,4 +2,7 @@ module "components" {
     source          =   "git::https://github.com/Vivekdevops1704/tf-module-vpc.git"
     for_each        =    var.vpc
     cidr            =    each.value["cidr"]
+    subnets          =    each.value["subnets"]
+
+
 }
